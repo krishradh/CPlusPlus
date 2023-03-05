8da28bae6f775b65232680a2997cec5323ef1773
krishradh <126420888+krishradh@users.noreply.github.com>
2023-03-06 01:42:02 +0530

    string to accept
    This file gives demo on usage to accept string

---
+++ string_accept.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//C program to accept a string
+#include <stdio.h>
+
+int main()
+{
+    
+    char str[50];
+    
+    printf("Enter any string\n");
+    scanf("%s",str);
+    
+    printf("String entered is -->%s",str);
+
+    return 0;
+}
